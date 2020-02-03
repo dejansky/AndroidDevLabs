5c8297319675e982433f2b0cbdf02fa0e6f8c337
Dejansky <dejanarsen@gmail.com>
2020-02-04 00:07:27 +0100

    halfway there

--- AndroidLabOne/app/src/main/java/com/example/androidlabone/CreateToDoActivity.kt
+++ AndroidLabOne/app/src/main/java/com/example/androidlabone/CreateToDoActivity.kt
@@ -1,12 +1,32 @@
 package com.example.androidlabone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import android.widget.EditText
 
 class CreateToDoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_to_do)
+
+        val title = findViewById<EditText>(
+            R.id.enterTitleOnCreate
+        ).editableText.toString()
+
+        val content = this.findViewById<EditText>(
+            R.id.enterContentOnCreate
+        ).editableText.toString()
+
+        val createBtn = findViewById<Button>(R.id.btnOnCreate)
+        createBtn.setOnClickListener {
+            toDoRepository.addToDo(title,content);
+            finish()
+        }
+
+
+
     }
 }
--- AndroidLabOne/app/src/main/java/com/example/androidlabone/MainActivity.kt
+++ AndroidLabOne/app/src/main/java/com/example/androidlabone/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.androidlabone
 
+import android.content.Context
+import android.content.Intent
+import android.content.LocusId
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewParent
+import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.FieldPosition
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = this.findViewById<ListView>(R.id.mainListView)
+
+        listView.adapter = ArrayAdapter<ToDo>(
+            this,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            toDoRepository.getAllToDos()
+        )
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                 val intent = Intent(this,ViewToDoActivity::class.java);
+                 startActivity(intent)
+        }
+
+        val createBtn = findViewById<Button>(R.id.btnOnMain)
+        createBtn.setOnClickListener {
+            val intent = Intent(this,CreateToDoActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+
+
+
+    }
+
 }

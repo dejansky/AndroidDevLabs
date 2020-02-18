d794ee483621504398741676792c94e8e9ce334d
Dejan Arsenijevic <arde1712@student.ju.se>
2020-02-18 11:49:47 +0100

    changes

--- AndroidLabOne/app/src/main/java/com/example/androidlabone/CreateToDoActivity.kt
+++ AndroidLabOne/app/src/main/java/com/example/androidlabone/CreateToDoActivity.kt
@@ -3,8 +3,12 @@ package com.example.androidlabone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.*
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_create_to_do.*
+import kotlinx.android.synthetic.main.activity_update_to_do.*
 
 class CreateToDoActivity : AppCompatActivity() {
 
@@ -14,7 +18,22 @@ class CreateToDoActivity : AppCompatActivity() {
 
 
 
-        val createBtn = findViewById<Button>(R.id.btnOnCreate) as Button
+        val createBtn = findViewById<Button>(R.id.btnOnCreate)
+
+        enterTitleOnCreate.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                createBtn.isEnabled = s.toString().trim() != ""
+            }
+        })
+
         createBtn.setOnClickListener {
             val title = findViewById<EditText>(
                 R.id.enterTitleOnCreate
--- AndroidLabOne/app/src/main/java/com/example/androidlabone/UpdateToDoActivity.kt
+++ AndroidLabOne/app/src/main/java/com/example/androidlabone/UpdateToDoActivity.kt
@@ -3,10 +3,13 @@ package com.example.androidlabone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_update_to_do.*
 
 class UpdateToDoActivity : AppCompatActivity() {
 
@@ -26,6 +29,20 @@ class UpdateToDoActivity : AppCompatActivity() {
 
         val saveBtn = findViewById<Button>(R.id.saveBtn)
 
+        newTitle.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                saveBtn.isEnabled = s.toString().trim() != ""
+            }
+        })
+
         saveBtn.setOnClickListener{
 
             val newTitle = findViewById<EditText>(
@@ -38,8 +55,6 @@ class UpdateToDoActivity : AppCompatActivity() {
 
             toDoRepository.updateToDoById(theId,newTitle,newContent)
 
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
             finish()
         }
     }
